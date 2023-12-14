c57a19bb727a673a09ba7ad0b3674b5fc55cc836
Jordan <jordansblackwell@gmail.com>
2023-12-14 09:50:40 -0500

    finished health app

---
+++ src/test/java/com/Blackwell/Jordan/healthapp/HealthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Blackwell.Jordan.healthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
